37696050bc21eebf855d42e230c7e455edc2cb24
Chris Randall <Chris.CRandall@outlook.com>
2023-12-02 15:25:23 -0600

    Added Contact in Home Controller

--- DevSiteMVC/Controllers/HomeController.cs
+++ DevSiteMVC/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using DevSiteMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using MimeKit;// Added for access to MimeMessage class
+using MailKit.Net.Smtp;// Added for access to SmtpClient class
 
 namespace DevSiteMVC.Controllers
 {
@@ -8,14 +10,71 @@ namespace DevSiteMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            var mm = new MimeMessage();
+
+            string message = $"You have received a new email from your site's contact form!<br/>" +
+                $"Sender: {cvm.Name}<br/>Email: {cvm.Email}<br/>Subject: {cvm.Subject}<br/>" +
+            $"Message: {cvm.Message}";
+
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            mm.Subject = cvm.Subject;
+            mm.Body = new TextPart("HTML") { Text = message };
+            mm.Priority = MessagePriority.Urgent;
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"));
+                client.Authenticate(
+                    _config.GetValue<string>("Credentials:Email:User"),
+                    _config.GetValue<string>("Credentials:Email:Password")
+                    );
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+
+                    ViewBag.ErrorMessage = $"There was an error processing your request." +
+                        $"Please try again later.<br/>" +
+                        $"Error Message: {ex.StackTrace}";
+
+                    return View(cvm);
+                }
+            }
+
+            return View("EmailConfirmation", cvm);
+
         }
 
         public IActionResult Privacy()
